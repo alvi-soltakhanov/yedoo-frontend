b6a2a0a645114723ae6755a586804b8e0d090070
Alvi-soltakhanov <Alvi-soltakanov@mail.ru>
2022-03-30 15:50:48 +0300

    Main

---
+++ src/redux/configureStore.js
@@ -0,0 +1,12 @@
+import { applyMiddleware, createStore, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { application } from "./features/application";
+
+
+export const store = createStore(
+    combineReducers({
+        application,
+    }),
+    composeWithDevTools(applyMiddleware(thunk))
+)
---
+++ src/redux/features/application.js
@@ -0,0 +1,12 @@
+const initialState = {
+    loading: false,
+    error: null,
+}
+
+export const application = (state = initialState, action) =>{
+    switch (action.type) {
+        default:
+    return state
+    }    
+}
+
