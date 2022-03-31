431a0e0a42ddcd4fd917175fa9a1b9262d357ea2
Yasin <yasabu@email.ru>
2022-03-31 14:31:03 +0300

    Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import style from "./header.module.css"
+import logo from '../../assets/Calling.png'
+import CartLine from '../../assets/CartLine.png'
+import location from "../../assets/Location.png"
+import search from "../../assets/Search.png"
+
+const Header = () => {
+    return (
+        <div className={style.header}>
+        <div className={style.content}>
+            <div className={style.name}>YEDOO</div>
+            <div className={style.inp}>
+                <img src={location} className={style.location} alt="" />
+                <input type="text" name="" id="" placeholder='Введите адрес доставки' />
+                <img src={search} className={style.search} alt="" />
+                </div>
+            <div className={style.contact}>
+               <div className={style.call}> <img src={logo} alt="" /></div>
+                <div className={style.iph}>
+                    <h5 className={style.phone}>Контакты: </h5>
+                    <span className={style.number}>+7(910)510-57-59</span>
+                </div>
+            </div>
+            <div className={style.cartBut}>
+               <div>Корзина</div>
+               <img src={CartLine} alt="" />
+               <div className={style.CartCount}><span>5</span></div>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import './index.css';
-import App from './components/App';
-import { store } from './redux/configureStore';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./components/App";
+import { store } from "./redux/configureStore";
+import Header from "./components/Header/Header";
 
 ReactDOM.render(
-    <React.StrictMode>  
-        <Provider store={store}>
-           <App />
-          
-        </Provider>
-      </React.StrictMode>,  
-  document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+      <Header />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
