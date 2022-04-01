6e31fa9717bc3ae7c9c523c63f349be7c90bc733
Alvi-soltakhanov <Alvi-soltakanov@mail.ru>
2022-04-01 14:17:13 +0300

    добавил fakedatebase для того чтобы настроить корзину

--- src/pages/CartPage/CartBlock/CartBlock.jsx
+++ src/pages/CartPage/CartBlock/CartBlock.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import Cart from '../Card/OneCard';
 import styles from './CartBlock.module.css'
 import LineHeight from '../../../assets/heigthLine.png'
+import arr from '../fakedateBase';
 
 const CartBlock = () => {
+    
     return (
-        <div className={styles.CartBlock}>
-            <div className={styles.CartBlockHeader}>
-                <div className={styles.returnBlock}> - к выбору блюда</div>
-                <div className={styles.Title}><img src={LineHeight} alt="" />КОРЗИНА <span>(в корзине 3 товара)</span></div>
+            // header страницы корзина
+            <div className={styles.CartBlock}>
+                <div className={styles.CartBlockHeader}>
+                    <div className={styles.returnBlock}> - к выбору блюда</div>
+                    <div className={styles.Title}><img src={LineHeight} alt="" />КОРЗИНА <span>(в корзине 3 товара)</span></div>
+                </div>
+
+            {/* Map карточек корзины  */}
+            {arr.map((item) => {
+                return <Cart image={item.image} title={item.title} composition={item.composition} count={item.count} price={item.price}/>
+            })}
+                
             </div>
-            <Cart />
-        </div>
     );
 };
 
