6274f8cc9e36fe1071bd1a8840bc76e710ab5c71
Alvi-soltakhanov <Alvi-soltakanov@mail.ru>
2022-04-01 10:22:56 +0300

    изменение стилей карточки

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./components/App";
 import { store } from "./redux/configureStore";
-import Header from "./components/Header/Header";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-      <Header />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
