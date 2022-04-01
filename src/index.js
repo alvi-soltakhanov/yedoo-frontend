372959915d66adc03d6e8543beb9bfb7a9497b07
Alvi-soltakhanov <Alvi-soltakanov@mail.ru>
2022-04-01 17:37:00 +0300

    верстка главной страницы

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import "./index.css";
-// import App from "./components/App";
+import App from "./components/App";
 import { store } from "./redux/configureStore";
-import MainPage from "./pages/MainPage/MainPage";
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <MainPage />
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
