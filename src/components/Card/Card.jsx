8f9989fb89ff3bd0f62f80479905fbecaad8544e
Yasin <yasabu@email.ru>
2022-04-01 10:48:36 +0300

    card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from "./card.module.css"
+import food from "../../assets/food.png"
+import cart from "../../assets/Buy.png"
+
+const Card = () => {
+    return (
+        <div className={styles.card}>
+            <div><img src={food} alt="" /></div>
+            <div className={styles.description}>
+                <div className={styles.nameAll}>
+                <p className={styles.name}>Индейка</p>
+                <p className={styles.weighth}>Вес: 225г</p>
+                </div>
+                <p className={styles.script}>Фаршированный гречневой кашей, курагой, апельсином и зеленым яблоком</p>
+                <div className={styles.price_script}>
+                    <p className={styles.price}>220₽</p>
+                    <button className={styles.cart}><img src={cart} alt="" />В корзину</button>
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import './index.css';
-import App from './components/App';
-import { store } from './redux/configureStore';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./components/App";
+import { store } from "./redux/configureStore";
+import Card from "./components/Card/Card";
 
 ReactDOM.render(
-    <React.StrictMode>  
-        <Provider store={store}>
-           <App />
-          
-        </Provider>
-      </React.StrictMode>,  
-  document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+      <Card />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
