7ccc089a121eef52965cf3d4521cb68f5d7b2324
unknown <ismailsadulaev1999@mail.ru>
2022-04-02 12:30:55 +0300

    Страница оформления заказа и поправки в большую карточку

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import "./index.css";
 import App from "./components/App";
 import { store } from "./redux/configureStore";
 import Header from "./components/Header/Header";
+import OrderRegistPage from "./pages/OrderRegistPage/OrderRegistPage";
+import FullCardPage from "./pages/FullCardPage/FullCardPage";
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      {/* <OrderRegistPage /> */}
+      <FullCardPage />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
