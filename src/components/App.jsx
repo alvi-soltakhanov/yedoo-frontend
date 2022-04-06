56ab8d9d238ef6595a2d67e0f8acc197594ad002
said-mohmad <sam-hp@mail.ru>
2022-04-06 16:27:01 +0300

    добавил санки для редактирования кафе, изменения в личном кабинете

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,50 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import CartPage from "../pages/CartPage/CartPage";
 import MainPage from "../pages/MainPage/MainPage";
 import FullCardPage from "../pages/FullCardPage/FullCardPage";
-import OrderRegistPage from '../pages/OrderRegistPage/OrderRegistPage'
+import OrderRegistPage from "../pages/OrderRegistPage/OrderRegistPage";
 import TermsPage from "../pages/DeliveryTermsPage/Terms";
 import ActionPage from "../pages/ActionPage/ActionPage";
 import "./App.css";
+import CafeProfile from "./CafeProfile/CafeProfile";
+import { useSelector } from "react-redux";
 
 const App = () => {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />}  />
-          <Route path="/cart" element={<CartPage />}  />
-          <Route path="/FullCard" element={<FullCardPage />}  />
-          <Route path="/TermsPage" element={<TermsPage />}  />
-          <Route path="/ActionPage" element={<ActionPage />}  />
-          <Route path="/OrderRegistPage" element={<OrderRegistPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
+    const token = localStorage.getItem("token");
+    // const token1 = useSelector(state => state.application.token);
+    if (token) {
+        return (
+            <div className="App">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<MainPage />}  />
+                        <Route path="/cart" element={<CartPage />}  />
+                        <Route path="/FullCard" element={<FullCardPage />}  />
+                        <Route path="/TermsPage" element={<TermsPage />}  />
+                        <Route path="/ActionPage" element={<ActionPage />}  />
+                        <Route path="/OrderRegistPage" element={<OrderRegistPage />} />
+                        <Route path="/CafeProfile" element={<CafeProfile />} />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        );
+    } else {
+        return (
+            <div className="App">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<MainPage />}  />
+                        <Route path="/cart" element={<CartPage />}  />
+                        <Route path="/FullCard" element={<FullCardPage />}  />
+                        <Route path="/TermsPage" element={<TermsPage />}  />
+                        <Route path="/ActionPage" element={<ActionPage />}  />
+                        <Route path="/OrderRegistPage" element={<OrderRegistPage />} />
+                        <Route path="/CafeProfile" element={<Navigate to="/" replace />} />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        );
+    }
+};
 
 export default App;
--- src/redux/features/cafe.js
+++ src/redux/features/cafe.js
@@ -2,10 +2,30 @@ const initialState = {
     loading: false,
     error: null,
     cafe: null,
+    cafeById: null,
 };
 
 export default function cafe(state = initialState, action) {
     switch (action.type) {
+        case "cafe/fetch/pending":
+            return {
+                ...state,
+                loading: true,
+                error: null,
+            };
+        case "cafe/fetch/rejected":
+            return {
+                ...state,
+                loading: true,
+                error: action.error,
+            };
+        case "cafe/fetch/fulfilled":
+            return {
+                ...state,
+                loading: false,
+                error: null,
+                cafe: action.payload,
+            };
         case "cafeById/fetch/pending":
             return {
                 ...state,
@@ -23,12 +43,76 @@ export default function cafe(state = initialState, action) {
                 ...state,
                 loading: false,
                 error: null,
-                cafe: action.payload
+                cafeById: action.payload,
+            };
+        case "cafe/edit/pending":
+            return {
+                ...state,
+                loading: true,
+                error: null,
+            };
+        case "cafe/edit/rejected":
+            return {
+                ...state,
+                loading: true,
+                error: action.error,
+            };
+        case "cafe/edit/fulfilled":
+            return {
+                ...state,
+                loading: false,
+                error: null,
+                cafe: state.cafe.map((cafe) => {
+                    if (cafe._id === action.payload._id) {
+                        cafe = action.payload;
+                    }
+                    return cafe;
+                }),
+            };
+            case "cafeByToken/fetch/pending":
+            return {
+                ...state,
+                loading: true,
+                error: null,
+            };
+        case "cafeByToken/fetch/rejected":
+            return {
+                ...state,
+                loading: true,
+                error: action.error,
+            };
+        case "cafeByToken/fetch/fulfilled":
+            return {
+                ...state,
+                loading: false,
+                error: null,
+                cafeById: action.payload
             };
         default:
             return state;
     }
 }
+
+export const fetchCafe = () => {
+    return async (dispatch) => {
+        dispatch({ type: "cafe/fetch/pending" });
+        try {
+            const res = await fetch(`http://localhost:4000/cafe`);
+            const json = await res.json();
+            if (json.error) {
+                dispatch({
+                    type: "cafe/fetch/rejected",
+                    error: json.error,
+                });
+            } else {
+                console.log(json);
+                dispatch({ type: "cafe/fetch/fulfilled", payload: json });
+            }
+        } catch (e) {
+            dispatch({ type: "cafe/fetch/rejected", error: e.toString() });
+        }
+    };
+};
 
 export const fetchCafeById = (cafeId) => {
     return async (dispatch) => {
@@ -46,6 +130,58 @@ export const fetchCafeById = (cafeId) => {
             }
         } catch (e) {
             dispatch({ type: "cafeById/fetch/rejected", error: e.toString() });
+        }
+    };
+};
+
+export const fetchCafeByToken = () => {
+    return async (dispatch) => {
+        dispatch({ type: "cafeByToken/fetch/pending" });
+        const token = localStorage.getItem('token')
+        try {
+            const res = await fetch(`http://localhost:4000/cafe/profile/user`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            const json = await res.json();
+            if (json.error) {
+                dispatch({
+                    type: "cafeByToken/fetch/rejected",
+                    error: json.error,
+                });
+            } else {
+                dispatch({ type: "cafeByToken/fetch/fulfilled", payload: json });
+            }
+        } catch (e) {
+            dispatch({ type: "cafeByToken/fetch/rejected", error: e.toString() });
+        }
+    };
+};
+
+export const editCafe = (cafeId, cafeName, cafeLogo, phone, mail, address, info) => {
+    return async (dispatch) => {
+        dispatch({ type: "cafe/edit/pending" });
+        const formData = new FormData();
+        cafeName && formData.append('name', cafeName);
+        phone && formData.append('phone', phone);
+        cafeLogo && formData.append('image', cafeLogo);
+        mail && formData.append('mail', mail);
+        address && formData.append('address', address);
+        
+        try {
+            const res = await fetch(`http://localhost:4000/cafe`, {
+                method: "PATCH",
+                body: formData
+            });
+            const json = await res.json();
+            if (json.error) {
+                dispatch({type: "cafe/edit/rejected", error: json.error});
+            } else {
+                dispatch({type: "cafe/edit/fulfilled", error: json.error});
+            }
+        } catch (e) {
+            dispatch({type: "cafe/edit/rejected", error: e.toString()})
         }
     };
 };
